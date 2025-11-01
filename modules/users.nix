1e7e75b5982c987de0d60d471ddfb67f7a806739
Dale Morgan <dale.morgan@me.com>
2025-11-01 17:35:15 -0500

    added ssh login

--- modules/users.nix
+++ modules/users.nix
@@ -9,5 +9,14 @@
     initialPassword = "xo";
     extraGroups    = [ "wheel" "systemd-journal" ];
     shell          = pkgs.bashInteractive;
+    # --- Add your SSH public keys here ---
+    # Option A: inline keys
+    openssh.authorizedKeys.keys = [
+      # Replace with your real public key(s):
+      "ssh-ed25519 123456"
+      # You can add more keys as needed
+      # "ssh-rsa AAAAB3Nz... another-key"
+    ];
+
   };
 }
