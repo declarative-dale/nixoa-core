856261b3950b33468ca9cb2d47f3c889180d0e67
Dale Morgan <mail@dalemorgan.us>
2026-01-26 02:57:31 -0600

    Align flake.nix with flake-file

--- flake.nix
+++ flake.nix
@@ -1,26 +1,9 @@
-# SPDX-License-Identifier: Apache-2.0
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
 {
   description = "NixOA-VM - Experimental Xen Orchestra Community Edition deployment for NixOS homelabs";
 
-  inputs = {
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
-    home-manager = {
-      url = "https://flakehub.com/f/nix-community/home-manager/0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-file.url = "github:vic/flake-file";
-    import-tree.url = "github:vic/import-tree";
-    xoSrc = {
-      url = "github:vatesfr/xen-orchestra/9b6d1089f4b96ef07d7ddc25a943c466e8c7bb4b";
-      flake = false;
-    };
-    libvhdiSrc = {
-      url = "https://github.com/libyal/libvhdi/releases/download/20240509/libvhdi-alpha-20240509.tar.gz";
-      flake = false;
-    };
-  };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./parts);
 
   nixConfig = {
     extra-substituters = [
@@ -33,5 +16,23 @@
     ];
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./parts);
+  inputs = {
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+    flake-file.url = "github:vic/flake-file";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "https://flakehub.com/f/nix-community/home-manager/0";
+    };
+    import-tree.url = "github:vic/import-tree";
+    libvhdiSrc = {
+      flake = false;
+      url = "https://github.com/libyal/libvhdi/releases/download/20240509/libvhdi-alpha-20240509.tar.gz";
+    };
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    xoSrc = {
+      flake = false;
+      url = "github:vatesfr/xen-orchestra/9b6d1089f4b96ef07d7ddc25a943c466e8c7bb4b";
+    };
+  };
 }
--- parts/nix/inputs/core.nix
+++ parts/nix/inputs/core.nix
@@ -3,6 +3,19 @@
 }:
 {
   # Base inputs for the NiXOA core flake.
+  flake-file.description = "NixOA-VM - Experimental Xen Orchestra Community Edition deployment for NixOS homelabs";
+
+  flake-file.nixConfig = {
+    extra-substituters = [
+      "https://install.determinate.systems"
+      "https://nixoa.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      "nixoa.cachix.org-1:N+GsSSd2yKgj2hx01fMG6Oe7tLfbxEi/V0oZFEB721g="
+    ];
+  };
+
   flake-file.inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
