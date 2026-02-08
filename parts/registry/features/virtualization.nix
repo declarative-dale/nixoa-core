ddbaf5743c3dccbf5bd6a6a5e71c2e2a916a9584
Dale Morgan <mail@dalemorgan.us>
2026-02-08 11:23:36 +0000

    fixed registry formatting

--- parts/registry/features/virtualization.nix
+++ parts/registry/features/virtualization.nix
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # Virtualization feature registry
-{ }:
+{ ... }:
 {
   modules = {
     xen-hardware = [
--- parts/registry/features/xo.nix
+++ parts/registry/features/xo.nix
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # XO feature registry
-{ }:
+{ ... }:
 {
   modules = {
     options = [
