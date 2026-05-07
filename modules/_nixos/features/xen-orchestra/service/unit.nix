a351b5271b399bd930feab7d2ba80b94159cfa2b
Dale Morgan <mail@dalemorgan.us>
2026-05-07 19:52:49 +0000

    Allow CIFS mount capability setup

--- modules/_nixos/features/xen-orchestra/service/unit.nix
+++ modules/_nixos/features/xen-orchestra/service/unit.nix
@@ -91,7 +91,9 @@ in {
           "CAP_NET_BIND_SERVICE"
           "CAP_SETUID"
           "CAP_SETGID"
+          "CAP_SETPCAP"
           "CAP_SYS_ADMIN"
+          "CAP_DAC_READ_SEARCH"
           "CAP_DAC_OVERRIDE"
         ];
 
