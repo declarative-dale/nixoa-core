124f7aef80a4ec5e25c0e75adcea9af318f90955
Dale Morgan <mail@dalemorgan.us>
2026-07-30 21:09:49 -0500

    Pin core to the fixed XO package

--- flake.nix
+++ flake.nix
@@ -18,6 +18,6 @@
     };
     import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
-    xen-orchestra-ce.url = "git+https://github.com/declarative-dale/xo-nixpkg.git?ref=refs/tags/latest";
+    xen-orchestra-ce.url = "git+https://github.com/declarative-dale/xo-nixpkg.git?ref=refs/heads/main";
   };
 }
